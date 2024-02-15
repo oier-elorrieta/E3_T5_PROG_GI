0a58d5c628c7a3d9cfb5354262ceeed0b485befc
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-15 14:02:16 +0100

    BezeroaDAO

---
+++ Erronka3/src/DAO/BezeroaDAO.java
@@ -0,0 +1,61 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Modelo.Bezeroa;
+
+public class BezeroaDAO {
+    private String url = "jdbc:mysql://localhost:3307/db_elorrietazinemaT5";
+    private String user = "root";
+    private String pass = "";
+
+    public Bezeroa[] getAllBezeroak() {
+        Bezeroa[] bezeroak = null;
+        int count = 0;
+
+        try (Connection con = DriverManager.getConnection(url, user, pass)) {
+            String countSql = "SELECT COUNT(*) AS total FROM BEZEROA";
+            try (PreparedStatement countPstmt = con.prepareStatement(countSql)) {
+                try (ResultSet countRs = countPstmt.executeQuery()) {
+                    if (countRs.next()) {
+                        count = countRs.getInt("total");
+                    }
+                }
+            }
+
+            bezeroak = new Bezeroa[count];
+
+            String sql = "SELECT * FROM BEZEROA";
+            try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    int index = 0;
+                    while (rs.next()) {
+                        int bezeroId = rs.getInt("Bezero_id");
+                        String izena = rs.getString("izena");
+                        String abizena = rs.getString("abizena");
+                        String nan = rs.getString("NAN");
+                        String sexua = rs.getString("Generoa");
+                        String email = rs.getString("Email");
+                        String telefonoa = rs.getString("telefonoa");
+                        String erabiltzailea = rs.getString("erabiltzailea");
+                        String pasahitza = rs.getString("pasahitza");
+                        String jaioData = rs.getString("jaio_data");
+
+
+                        Bezeroa bezeroa = new Bezeroa(izena, abizena, nan, sexua, pasahitza, erabiltzailea);
+                        bezeroak[index] = bezeroa;
+                        index++;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return bezeroak;
+    }
+}
--- Erronka3/src/Modelo/DatuBase.java
+++ Erronka3/src/Modelo/DatuBase.java
@@ -3,38 +3,40 @@ package Modelo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import DAO.BezeroaDAO;
 
 public class DatuBase {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-  public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		// Driver kargatu
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// Konexioa sortu
-			String url = "jdbc:mysql://localhost:3307/db_elorrietazinemaT5";
-			String user = "root";
-			String pass = "";
-			
-			
-				Connection con = DriverManager.getConnection(url, user, pass);
-				
-		
-		// Itxi dena ordenean
-		con.close();
-				
-		}catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();	
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-  
+            String url = "jdbc:mysql://localhost:3307/db_elorrietazinemaT5";
+            String user = "root";
+            String pass = "";
 
+            Connection con = DriverManager.getConnection(url, user, pass);
+            con.close();
+
+            BezeroaDAO bezeroaDAO = new BezeroaDAO();
+            Bezeroa[] bezeroak = bezeroaDAO.getAllBezeroak();
+
+            for (Bezeroa bezeroa : bezeroak) {
+                System.out.println("Izena: " + bezeroa.getIzena());
+                System.out.println("Abizena: " + bezeroa.getAbizena());
+                System.out.println("NAN: " + bezeroa.getNan());
+                System.out.println("Sexua: " + bezeroa.getSexua());
+                System.out.println("Erabiltzailea: " + bezeroa.getErabiltzailea());
+                System.out.println("Pasahitza: " + bezeroa.getPasahitza());
+                System.out.println("---------------------------------------------");
+            }
+        
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
