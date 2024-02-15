2bc1331a5a0dab0305c70d6f376f413443a40a20
miraricasillas <116722326+miraricasillas@users.noreply.github.com>
2024-02-15 12:35:42 +0100

    Update DatuBase.java

--- Erronka3/src/Modelo/DatuBase.java
+++ Erronka3/src/Modelo/DatuBase.java
@@ -2,4 +2,35 @@ package Modelo;
 
 public class DatuBase {
 
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Driver kargatu
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// Konexioa sortu
+			String url = "jdbc:mysql://localhost:3307/db_elorrietazinemaT5";
+			String user = "root";
+			String pass = "";
+			
+			
+				Connection con = DriverManager.getConnection(url, user, pass);
+				
+		
+		// Itxi dena ordenean
+		con.close();
+				
+		}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();	
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+  
+
 }
