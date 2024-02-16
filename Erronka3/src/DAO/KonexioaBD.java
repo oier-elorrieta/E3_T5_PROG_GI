1ee495ad60fdaa20411fb6494055da9c6fa35eb7
Jonnaan <116714200+Jonnaan@users.noreply.github.com>
2024-02-16 13:01:35 +0100

    DAOK eginda eta konexioa

---
+++ Erronka3/src/DAO/KonexioaBD.java
@@ -0,0 +1,39 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class KonexioaBD {
+    private String url = "jdbc:mysql://localhost:3307/db_elorrietazinemaT5";
+    private String user = "root";
+    private String pass = "";
+    private Connection connection;
+
+    public KonexioaBD() {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(url, user, pass);
+            System.out.println("Conexión establecida correctamente.");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void desconectar() {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Conexión cerrada correctamente.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
