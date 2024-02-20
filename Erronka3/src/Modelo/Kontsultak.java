d0e5a639b2442cf7e9a5c085311df4eea59981a0
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-20 13:31:01 +0100

    Filme pantaila sortuta eta lortuta

--- Erronka3/src/Modelo/Kontsultak.java
+++ Erronka3/src/Modelo/Kontsultak.java
@@ -11,24 +11,21 @@ import DAO.ZinemakDAO;
 
 public class Kontsultak {
     private KonexioaBD konexioaBD;
-    private Zinema aukeratuZinema;
-
+    
     public Kontsultak(KonexioaBD konexioaBD) {
         this.konexioaBD = konexioaBD;
     }
 
-    public void setAukeratuZinema(Zinema zinema) {
-        this.aukeratuZinema = zinema;
-    }
+ 
 
-    public String[] FilmakZinema(String selectedZinemaId) {
+    public String[] FilmakZinema(String selectedZinema) {
         String[] filmakZinema = null;
         int i = 0;
         int count = 0;
         try (Connection con = konexioaBD.getConnection()) {
             String countSql = "SELECT COUNT(*) AS total FROM filma f JOIN saioa s ON f.filma_id = s.filma_id WHERE s.zinema_id = ?";
             try (PreparedStatement sta = con.prepareStatement(countSql)) {
-                sta.setString(1, selectedZinemaId);
+                sta.setString(1, selectedZinema);
                 try (ResultSet res = sta.executeQuery()) {
                     if (res.next()) {
                         count = res.getInt("total");
@@ -44,7 +41,7 @@ public class Kontsultak {
                     "WHERE s.zinema_id = ? " +
                     "ORDER BY ABS(DATEDIFF(s.eguna, CURDATE()))";
             try (PreparedStatement sta = con.prepareStatement(sql)) {
-                sta.setString(1, selectedZinemaId);
+                sta.setString(1, selectedZinema);
                 try (ResultSet rs = sta.executeQuery()) {
                     while (rs.next()) {
                         filmakZinema[i++] = rs.getString("izena");
