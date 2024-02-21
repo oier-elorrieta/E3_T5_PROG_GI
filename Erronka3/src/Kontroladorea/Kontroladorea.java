49977690856e0a56a0388e5fbc7bde8c7b933cf9
jonnaan <jonander.fernandezdo@elorrieta-errekamari.com>
2024-02-21 16:36:01 +0100

    Jdatapicker sartu eta kontroladorea actualizatuta

--- Erronka3/src/Kontroladorea/Kontroladorea.java
+++ Erronka3/src/Kontroladorea/Kontroladorea.java
@@ -1,18 +1,26 @@
 package Kontroladorea;
 
+import DAO.SaioaDAO;
+import DAO.ZinemakDAO;
+import Modelo.Saioa;
+import Modelo.Zinema;
 import Vista.OngiEtorri;
 
 public class Kontroladorea {
 
-	public static void main(String[] args) {
-	
-		try {
-            OngiEtorri frame = new OngiEtorri();
+    public static void main(String[] args) {
+        ZinemakDAO zinemakDAO = new ZinemakDAO();
+        SaioaDAO  saioaDAO = new SaioaDAO();
+        Zinema[] zinemakList = zinemakDAO.getAllZinemak();
+        Saioa[] saioaList = saioaDAO.getAllSaioak();
+    
+        try {
+            OngiEtorri frame = new OngiEtorri(zinemakList, saioaList);
             frame.setVisible(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-	}
+    }
 
 }
